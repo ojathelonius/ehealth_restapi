5b5876c6c7dedd74d741d64b17992fad2eb0188f
ojathelonius <louis.johanet@epfedu.fr>
2016-11-07 11:16:40 +0100

    Prevented writing to db while the collection isn't ready Using the config file

--- app.js
+++ app.js
@@ -1,58 +1,61 @@
 var express = require('express');
 var mongoClient = require('mongodb').MongoClient;
 var mqtt = require('mqtt');
+var config = require('./node_subscriber_config');
 
-//Init app
+// Init app
 var app = express();
 
-//Set pot to listen t
+// Set port to listen
 var port = process.env.PORT || 3000;
 
-//Declare db
-var sensorDB;
+// Declare db
+var sensorDB = false;
+
+var connection_status;
 
 //Mongolab URL
-var mongodbUrl = 'mongodb://admin:admin@ds143767.mlab.com:43767/esante';
+var mongodbUrl = 'mongodb://ehealth:ehealth@ds063856.mlab.com:63856/ojathelonius';
 
 //Connect to db
-mongoClient.connect(mongodbUrl, function(err, db) {
-  if(!err) {
-    console.log('MongoDB connected');
-    mongoCollection = db.createCollection('rasp_sensor', {strict:true}, function(err, collection) {});
-    sensorDB = db;
-  }
-});
-
-//Set the IP
-var client = mqtt.connect('mqtt://ip_cible');
-
-
-client.on('connect', function () {
-  client.subscribe('presence');
-});
-
-client.on('message', function (topic, message) {
-  // message is Buffer
-  data = message.toString();
-  dataObj = {
-    measure: data
-  };
-
-  insertSensorData(sensorDB, dataObj, function(result) {});
-    console.log(message.toString());
-});
-
-//Push data to db function
-var insertSensorData = function(db, data, cb) {
-  db.collection('rasp_sensor').insertOne(data, function(err, result) {
+mongoClient.connect(config.mongodb_info.url, function(err, db) {
     if (!err) {
-      console.log('Insert data to db succeeded');
-    } else {
-      console.log('Failed to insert data.')
+        console.log('MongoDB connected');
+        mongoCollection = db.createCollection(config.mongodb_info.collection, {
+            strict: true
+        }, function(err, collection) {});
+        sensorDB = db;
     }
-  });
+});
+
+var client = mqtt.connect("mqtt://"+config.broker_info.url+":"+config.broker_info.port.toString(), config.broker_info.options);
+
+
+client.on('connect', function() {
+    client.subscribe(config.broker_info.topic);
+    connection_status = true;
+});
+
+client.on('message', function(topic, message) {
+    if(connection_status && sensorDB) {
+        data = JSON.parse(message.toString());
+        insertSensorData(sensorDB, data, function(result) {});
+    }else{
+      console.log('Connection or collection not ready yet.')
+    }
+
+});
+
+var insertSensorData = function(db, data, cb) {
+    db.collection('rasp_sensor').insertOne(data, function(err, result) {
+        if (!err) {
+            console.log('Insert data to db succeeded');
+        } else {
+            console.log('Failed to insert data.')
+        }
+    });
 };
 
-app.listen(port, function () {
-  console.log('Listenning on port 3000');
+app.listen(port, function() {
+    console.log('Listening on port 3000');
 });
