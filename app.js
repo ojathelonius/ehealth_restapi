8e8c83e37c37d70559739cd54d924fcc27b3123c
ojathelonius <louis.johanet@epfedu.fr>
2016-11-08 12:43:03 +0100

    Added token authentification

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var mongoClient = require('mongodb').MongoClient;
 var mqtt = require('mqtt');
-var config = require('./node_subscriber_config');
+var subscriber_config = require('./node_subscriber_config');
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var jwt    = require('jsonwebtoken');
+var User   = require('./models/user');
 
 // Init app
 var app = express();
@@ -14,25 +19,22 @@ var sensorDB = false;
 
 var connection_status;
 
-//Mongolab URL
-var mongodbUrl = 'mongodb://ehealth:ehealth@ds063856.mlab.com:63856/ojathelonius';
-
 //Connect to db
-mongoClient.connect(config.mongodb_info.url, function(err, db) {
+mongoClient.connect(subscriber_config.mongodb_info.url, function(err, db) {
     if (!err) {
         console.log('MongoDB connected');
-        mongoCollection = db.createCollection(config.mongodb_info.collection, {
+        mongoCollection = db.createCollection(subscriber_config.mongodb_info.collection, {
             strict: true
         }, function(err, collection) {});
         sensorDB = db;
     }
 });
 
-var client = mqtt.connect("mqtt://"+config.broker_info.url+":"+config.broker_info.port.toString(), config.broker_info.options);
+var client = mqtt.connect("mqtt://"+subscriber_config.broker_info.url+":"+subscriber_config.broker_info.port.toString(), subscriber_config.broker_info.options);
 
 
 client.on('connect', function() {
-    client.subscribe(config.broker_info.topic);
+    client.subscribe(subscriber_config.broker_info.topic);
     connection_status = true;
 });
 
@@ -56,6 +58,110 @@ var insertSensorData = function(db, data, cb) {
     });
 };
 
-app.listen(port, function() {
-    console.log('Listening on port 3000');
+
+mongoose.connect(subscriber_config.mongodb_info.url); // connect to database
+app.set('superSecret', subscriber_config.mongodb_info.secret); // secret variable
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
+
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
+
+
+app.get('/setup', function(req, res) {
+
+  var nick = new User({
+    name: 'Nick Cerminara',
+    password: 'password',
+    admin: true
+  });
+
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+var apiRoutes = express.Router();
+
+
+apiRoutes.post('/authenticate', function(req, res) {
+
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresIn: 1440
+        });
+
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+});
+
+apiRoutes.use(function(req, res, next) {
+
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if (token) {
+
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+
+  }
+});
+
+
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+
+app.use('/api', apiRoutes);
