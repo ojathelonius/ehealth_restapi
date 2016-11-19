2cf22c6fea052c5b8ea2ed631d294e3c23a2874f
ojathelonius <louis.johanet@epfedu.fr>
2016-11-19 17:36:37 +0100

    Removed token from User schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,5 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model('User', new Schema({
     client_id: String,
     password: String,
-    admin: Boolean,
-    token: String
+    admin: Boolean
 }));
