13956ffa6f202435544edfd82449852960c2c009
ojathelonius <louis.johanet@epfedu.fr>
2016-12-06 23:31:41 +0100

    added user info

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('User', new Schema({
-    client_id: String,
-    password: String,
-    admin: Boolean
+  client_id: String,
+  password: String,
+  admin: Boolean,
+  firstname: String,
+  lastname: String,
+  age: Number,
+  email: String
 }));
